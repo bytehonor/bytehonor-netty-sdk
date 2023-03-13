fac13ce36d43abab8263490288589f7cf5dfc6dc
lijianqiang <343891954@qq.com>
2023-03-13 20:00:15 +0800

    test

--- src/test/java/com/bytehonor/sdk/beautify/netty/client/NettyClientTest.java
+++ src/test/java/com/bytehonor/sdk/beautify/netty/client/NettyClientTest.java
@@ -1,26 +1,23 @@
 package com.bytehonor.sdk.beautify.netty.client;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.bytehonor.sdk.beautify.netty.common.handler.NettyMessageSender;
 import com.bytehonor.sdk.beautify.netty.common.model.NettyMessage;
-import com.bytehonor.sdk.beautify.netty.common.task.NettySleeper;
+import com.bytehonor.sdk.beautify.netty.common.model.NettyPayload;
 
 public class NettyClientTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(NettyClientTest.class);
 
-    @Test
-    public void test() {
-        boolean isOk = true;
-        NettyClient client = new NettyClient("127.0.0.1", 85, new NettyClientHandler() {
+    public static void main(String[] args) {
+        NettyClient client = new NettyClient("www.bytehonor.com", 85, new NettyClientHandler() {
 
             @Override
             public void onOpen(String stamp) {
-                LOG.info("onOpen");
+                LOG.info("onOpen, stamp:{}", stamp);
+                NettyMessageSender.send(stamp, NettyPayload.of("hello server"));
             }
 
             @Override
@@ -35,19 +32,13 @@ public class NettyClientTest {
 
             @Override
             public void onMessage(NettyMessage message) {
-                
+
             }
 
         });
-        try {
-            client.run();
-            NettySleeper.sleep(15000L);
-        } catch (Exception e) {
-            LOG.error("error", e);
-            isOk = false;
-        }
+        LOG.info("run");
+        client.run();
 
-        assertTrue("test", isOk);
     }
 
 }
